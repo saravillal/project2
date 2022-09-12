cbe7aa4d13aa6c28133fc6c703086d47d26e4c88
Your Name <you@example.com>
2022-09-12 23:57:39 +0200

    components

--- todolist/src/App.vue
+++ todolist/src/App.vue
@@ -1,10 +1,37 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
   </nav>
   <router-view/>
 </template>
+
+<script>
+import userStore from '@/store/user';
+import { mapState, mapActions } from 'pinia';
+
+export default {
+  name: 'App',
+  computed: {
+    ...mapState(userStore, ['user']),
+  },
+  methods: {
+    ...mapActions(userStore, ['fetchUser']),
+  },
+  async created() {
+    try {
+      await this.fetchUser();
+      if (!this.user) {
+        this.$router.push({ path: '/auth' });
+      } else {
+        this.$router.push({ path: '/' });
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  },
+};
+
+</script>
 
 <style>
 #app {
---
+++ todolist/src/views/AuthView.vue
@@ -0,0 +1,34 @@
+<template>
+  <h1>Esta es la vista de SignUp/In</h1>
+  <button @click="signUp">SignUp</button>
+</template>
+
+<script>
+import { mapState, mapActions } from 'pinia';
+import userStore from '@/store/user';
+
+export default {
+  name: 'AuthView',
+  computed: {
+    ...mapState(userStore, ['user']),
+  },
+  methods: {
+    ...mapActions(userStore, ['signUp']),
+    signUp() {
+      const userData = {
+        email: 'nacho.martinez@ironhack.com',
+        password: 'pruebasignup',
+      };
+      this.signUp(userData.email, userData.password);
+    },
+  },
+  watch: {
+    user() {
+      if (this.user) {
+        console.log(this.user);
+        this.$router.push({ path: '/' });
+      }
+    },
+  },
+};
+</script>
